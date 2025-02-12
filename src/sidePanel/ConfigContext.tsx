85c2cf57187c1ae1503ab0f9e877581bc6596613
3-ark <first2wood@gmail.com>
2025-02-12 13:46:49 +0800

    Refactor personas structure in ConfigContext for improved readability and maintainability

--- src/sidePanel/ConfigContext.tsx
+++ src/sidePanel/ConfigContext.tsx
@@ -2,10 +2,24 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export const ConfigContext = createContext({});
 
-export const personas = { sidellama: 'You are a meticulous academic proficient in analyzing research papers: - Concisely restate the core problem statement(s); - Summarize the central arguments and key findings, list the data and facts particularly; - Extract the primary takeaways and their implications; - Formulate 3 insightful questions stemming from the paper, and provide well-reasoned answers based on the text. Avoid speculative answers not supported by the provided content.
-'};
+export const personas = {
+  sidellama: `You are a meticulous academic proficient in analyzing research papers:
+  - Concisely restate the core problem statement(s);
+  - Summarize the central arguments and key findings, list the data and facts particularly;
+  - Extract the primary takeaways and their implications;
+  - Formulate 3 insightful questions stemming from the paper, and provide well-reasoned answers based on the text.
+  Avoid speculative answers not supported by the provided content.`,
+};
 
-const defaultConfig = { personas, generateTitle: true, backgroundImage: true, persona: 'sidellama', webMode: 'brave', webLimit: 10, contextLimit: 10 };
+const defaultConfig = {
+  personas,
+  generateTitle: true,
+  backgroundImage: true,
+  persona: 'sidellama',
+  webMode: 'brave',
+  webLimit: 10,
+  contextLimit: 10,
+};
 
 export const ConfigProvider = ({ children }: any) => {
   const initialConfig = JSON.parse(localStorage.getItem('config') || JSON.stringify(defaultConfig));
